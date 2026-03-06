b75f473ee877c2b6cf4c53e0fad092cbde355b6e
John <fora-glimlacht-0t@icloud.com>
2026-03-06 09:58:03 +0100

    update rename image skill

--- src/config/data/heroSideImageSectionData.json.ts
+++ src/config/data/heroSideImageSectionData.json.ts
@@ -2,7 +2,7 @@ import {
   type LocalizedData,
   type HeroSideImageSectionData,
 } from "../translations/configDataTypes.ts";
-import heroImage from "@images/enricos_transport_service-bus-zonsopgang_4x3.jpg";
+import heroImage from "@images/enricos-transport-service-bus-zonsopgang-4x3.jpg";
 import { textTranslations } from "@config/translations/textTranslations.ts";
 import { ROUTES } from "@config/constants.ts";
 
@@ -12,8 +12,8 @@ export const heroSideImageSectionData: LocalizedData<HeroSideImageSectionData> =
       src: heroImage,
       alt: "Enrico Janssen in bestelbus",
     },
-    title: "Koerier in Zwolle – Snel, veilig en flexibel{highlight}",
-    description: "Uw betrouwbare koerier in Zwolle en omgeving",
+    title: "Helder, snel{highlight} en zonder ver\u00ADras\u00ADsingen",
+    description: "Direct inzicht in kosten",
     ctaButton: {
       href: ROUTES.nl.offerteAanvragen,
       text: textTranslations.nl.get_quote_button_text_short,
@@ -24,8 +24,8 @@ export const heroSideImageSectionData: LocalizedData<HeroSideImageSectionData> =
       src: heroImage,
       alt: "Enrico Janssen sitting in a delivery van",
     },
-    title: "Courier in Zwolle – Fast, safe and flexible{highlight}",
-    description: "Reliable carrier in Zwolle and surrounding areas",
+    title: "Clear, fast{highlight} and without surprises",
+    description: "Immediate cost insight",
     ctaButton: {
       href: ROUTES.en.offerteAanvragen,
       text: textTranslations.en.get_quote_button_text_short,
