25156e1980fe6c1d726caa1f51c15aa01fc9fd5a
John <fora-glimlacht-0t@icloud.com>
2026-03-09 20:33:49 +0100

    wip...

--- src/config/routeTranslations.ts
+++ src/config/routeTranslations.ts
@@ -4,25 +4,15 @@
 
 export const routeTranslations = {
   nl: {
-    /**** pages ****/
     "bezorggebieden*": "bezorggebieden/*",
     "blog*": "blog/*",
     "categorieen*": "categorieen/*",
     "diensten*": "diensten/*",
-    contact: "contact",
-    offerte_aanvragen: "offerte-aanvragen",
-    over_ons: "over-ons",
-    partner_worden: "partner-worden",
   },
   en: {
-    /**** pages ****/
     "bezorggebieden*": "delivery-areas/*",
     "blog*": "blog/*",
     "categorieen*": "categories/*",
     "diensten*": "services/*",
-    contact: "contact",
-    offerte_aanvragen: "request-quote",
-    over_ons: "about-us",
-    partner_worden: "become-a-partner",
   },
 } as const;
