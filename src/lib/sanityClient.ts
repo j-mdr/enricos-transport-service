f6b3a215786d76861d549e94e6177f06f61d30fe
John <fora-glimlacht-0t@icloud.com>
2026-03-17 14:05:15 +0100

    fixing.....2

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID ?? process.env.PUBLIC_SANITY_PROJECT_ID ?? "",
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID ?? process.env.PUBLIC_SANITY_PROJECT_ID ?? "p88mnbf2",
   dataset:
     import.meta.env.PUBLIC_SANITY_DATASET ?? process.env.PUBLIC_SANITY_DATASET ?? "production",
   apiVersion: "2025-03-13",
